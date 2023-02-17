4770a62f5addb46003257e8e4744d3d36581c7cc
jonsori <yohanees53@gmail.com>
2023-02-17 09:21:12 -0800

    upload

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers of base16 in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 102)
+	{
+		putchar(digit);
+
+		if (num == 57)
+			num += 39;
+		++num;
+	}
+	putchar('\n');
+
+	return (0);
+}
