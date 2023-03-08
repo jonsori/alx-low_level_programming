6b2fea2963877494a1368e88a3e313c0c96d6e08
jonsori <yohanees53@gmail.com>
2023-03-08 14:40:35 -0800

    Update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,6 +3,9 @@
 /**
  * str_checker - check if two strings are identical.
  * @s1: string_1 base address.
+ * @s2: string_2 base address.
+ * @i: left index.
+ * @j: special index. (joker)
  * Return: 1 if s is palindrome, 0 otherwise.
  */
 int str_checker(char *s1, char *s2, int i, int j)
